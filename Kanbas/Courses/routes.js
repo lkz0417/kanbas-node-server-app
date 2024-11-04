02eba10b5876fa4b0317678a166965f899772449
lkz0417 <liukuizi@163.com>
2024-11-04 14:11:00 -0800

    a5fix

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,4 +1,5 @@
 import Database from "../Database/index.js";
+import * as dao from "./dao.js";
 export default function CourseRoutes(app) {
   app.get("/api/courses", (req, res) => {
     const courses = Database.courses;
@@ -12,20 +13,33 @@ export default function CourseRoutes(app) {
     res.send(course);
   });
 
-  app.delete("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    Database.courses = Database.courses.filter((c) => c._id !== id);
+  // app.delete("/api/courses/:id", (req, res) => {
+  //   const { id } = req.params;
+  //   Database.courses = Database.courses.filter((c) => c._id !== id);
+  //   res.sendStatus(204);
+  // });
+  app.delete("/api/courses/:courseId", (req, res) => {
+    const { courseId } = req.params;
+    dao.deleteCourse(courseId);
     res.sendStatus(204);
   });
 
-  app.put("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    const course = req.body;
-    Database.courses = Database.courses.map((c) =>
-        c._id === id ? { ...c, ...course } : c
-    );
+
+  // app.put("/api/courses/:id", (req, res) => {
+  //   const { id } = req.params;
+  //   const course = req.body;
+  //   Database.courses = Database.courses.map((c) =>
+  //       c._id === id ? { ...c, ...course } : c
+  //   );
+  //   res.sendStatus(204);
+  // });
+  app.put("/api/courses/:courseId", (req, res) => {
+    const { courseId } = req.params;
+    const courseUpdates = req.body;
+    dao.updateCourse(courseId, courseUpdates);
     res.sendStatus(204);
   });
+
 
 
 }
