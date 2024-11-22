7a656941c36f0f593f886e2e1f13a1467ea2bfdb
lkz0417 <liukuizi@163.com>
2024-11-22 12:51:49 -0800

    fix db connection

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/route.js";
 import cors from "cors";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017" + "/kanbas";
+
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
